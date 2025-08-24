8674fc5e553223eb431452f0a7542b2d95dbef32
Mher Saratikyan <91123731+sarat1kyan@users.noreply.github.com>
2025-08-24 20:52:03 +0400

    Delete backend/tests directory

--- backend/tests/test_dropbox_search.py
+++
@@ -1,6 +0,0 @@
-from app.services.dropbox_search import search_similar_files
-
-def test_dropbox_search_no_token():
-    res = search_similar_files("er_sol_rou_14kw.3dm")
-    assert isinstance(res, list) and len(res) >= 1
-    assert "filename" in res[0]
